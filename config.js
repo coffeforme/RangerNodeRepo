84c9b3913994229b1e97508dcc6daaf65ae65d02
coffeforme <coffeforme@outlook.com>
2018-12-21 19:02:32 -0500

    Se agrega Procfile y se modifica el package

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || "mongodb://admin:clave123@ranger-shard-00-00-46sbq.mongodb.net:27017,ranger-shard-00-01-46sbq.mongodb.net:27017,ranger-shard-00-02-46sbq.mongodb.net:27017/test?ssl=true&replicaSet=Ranger-shard-0&authSource=admin&retryWrites=true",
+    db: process.env.MONGODB || "mongodb://admin:clave123@ranger-shard-00-00-46sbq.mongodb.net:27017,ranger-shard-00-01-46sbq.mongodb.net:27017,ranger-shard-00-02-46sbq.mongodb.net:27017/rangerdb?ssl=true&replicaSet=Ranger-shard-0&authSource=admin&retryWrites=true",
     SECRET_TOKEN: 'RHCP'
 }
