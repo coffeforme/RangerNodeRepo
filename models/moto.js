522e62e0a350cf854615c33181c2562b9db7d22d
coffeforme <coffeforme@outlook.com>
2018-12-24 13:30:58 -0500

    prifile add

--- models/moto.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
-const MotoSchema = Schema({
-    marca: { type: String },
-    modelo: { type: String }
-})
-module.exports = mongoose.model('moto', MotoSchema)
--- models/usuario.js
+++ models/usuario.js
@@ -1,22 +1,34 @@
 'use strict'
 
 const mongoose = require('mongoose')
-const ProfileSchema = require('./profile')
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt')
 const crypto = require('crypto')
 
-const UsuarioSchema = Schema({
+const motoSchema = Schema({
+    marca: { type: String },
+    modelo: { type: String }
+})
+
+const perfilSchema = Schema({
+    name: { type: String },
+    birth: { type: Date },
+    moto: { type: Schema.Types.ObjectId, ref: 'moto' },
+    dateCreated: { type: Date, default: Date.now() },
+    lastDate: { type: Date }
+})
+
+const usuarioSchema = Schema({
     email: { type: String, unique: true, lowercase: true },
     user: { type: String, unique: true, lowercase: true },
-    profile: { type: ProfileSchema },
+    profile: { type: Schema.Types.ObjectId, ref: 'profile' },
     avatar: String,
     pass: { type: String, select: false },
     fechaCreacion: { type: Date, default: Date.now() },
     ultimoIngreso: Date
 })
 
-UsuarioSchema.pre('save', function (next) {
+usuarioSchema.pre('save', function (next) {
     let user = this
     if (!user.isModified('pass')) return next()
     bcrypt.genSalt(10, (err, salt) => {
@@ -29,7 +41,7 @@ UsuarioSchema.pre('save', function (next) {
     })
 })
 
-UsuarioSchema.methods.gravatar = function () {
+usuarioSchema.methods.gravatar = function () {
     if (!this.pass) return "https://gravatar.com/avatar?s=200&d=retro"
 
     const md5 = crypto.createHash('md5').update(this.email).digest('hex')
@@ -37,10 +49,12 @@ UsuarioSchema.methods.gravatar = function () {
     return `https://gravatar.com/avatar/${md5}?s=200&d=retro`;
 }
 
-UsuarioSchema.methods.comparePassword = function (candidatePassword, cb) {
+usuarioSchema.methods.comparePassword = function (candidatePassword, cb) {
     bcrypt.compare(candidatePassword, this.pass, (err, isMatch) => {
         cb(err, isMatch)
     });
 }
-
-module.exports = mongoose.model('users', UsuarioSchema)
+const Moto = mongoose.model('moto', motoSchema);
+const Profile = mongoose.model('profile', perfilSchema);
+const User = mongoose.model('user', usuarioSchema);
+module.exports = User
